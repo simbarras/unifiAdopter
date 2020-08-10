c6d0250f13bd36e816459a634f9c9d13026fb16c
Simon Barras <66463606+simbarras@users.noreply.github.com>
2020-08-10 16:24:41 +0200

    Add files via upload

---
+++ workers/Ssh.py
@@ -0,0 +1,47 @@
+import paramiko
+
+
+class Ssh:
+    ip = ''
+    port = 22
+    username = ''
+    password = ''
+    url = ''
+    urlPort = 0
+
+    def __init__(self, controllerAntenna):
+        self.username = controllerAntenna.user
+        self.password = controllerAntenna.mdp
+        self.url = controllerAntenna.url
+        self.urlPort = controllerAntenna.port
+
+    def addtocontroller(self, ip):
+        self.ip = ip
+        # self.ip = '10.11.100.53'
+
+        try:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(self.ip, self.port, self.username, self.password)
+
+            stdin, stdout, stderr = ssh.exec_command(
+                "/usr/bin/mca-cli-op set-inform http://unifi." + self.url + ":" + str(self.urlPort) + "/inform")
+
+            outlines = stdout.readlines()
+            outErrors = stderr.readlines()
+            if not outlines == []:
+                resp = ''.join(outlines)
+                return 'Ok'
+            elif not outErrors == []:
+                errors = ''.join(outErrors)
+                return 'Errors: ' + errors
+            else:
+                return 'No response'
+        except TimeoutError:
+            return 'Empty'
+        except paramiko.ssh_exception.NoValidConnectionsError:
+            return 'Ignored'
+        except paramiko.ssh_exception.BadAuthenticationType:
+            return 'Ignored'
+        except paramiko.ssh_exception.AuthenticationException:
+            return 'Ignored'
