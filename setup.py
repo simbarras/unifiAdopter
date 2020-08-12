f88e05dba434ade8d34c78398b2d322a8bbc1d47
simbarras <simon.barras02@gmail.com>
2020-08-12 16:12:43 +0200

    Form package for pip

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='unifiAdopter',
+    version='2.5.1',
+    description='Python project who add automaticly all antenna from a subnet to a distant controller',
+    py_modules=["unifiAdopter"],
+    package_dir={'': 'src'},
+)
