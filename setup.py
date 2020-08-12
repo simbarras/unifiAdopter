04cf1535aa420c2a557b2abbe1cf9e9b63a1c47a
simbarras <simon.barras02@gmail.com>
2020-08-12 16:54:57 +0200

    Form package for pip

--- setup.py
+++ setup.py
@@ -4,10 +4,21 @@ setuptools.setup(
     name='unifiAdopter',
     version='2.5.1',
     description='Python project who add automaticly all antenna from a subnet to a distant controller',
-    py_modules=["unifiAdopter", "paramiko"],
+    py_modules=["unifiAdopter"],
     package_dir={'': 'src'},
     author="Simon Barras",
     author_email="simon.barras02@gmail.com",
     url="https://github.com/simbarras/unifiAdopter",
     scripts=['unifiAdopter.py'],
+    install_requires=["paramiko >= 2.7.1"],
+    extras_requires={"dev": [
+        "pytest>=3.7", ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
