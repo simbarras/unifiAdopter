203ce27ed842a18296307f3790b4baca8e5e5218
simbarras <simon.barras02@gmail.com>
2020-08-13 12:01:19 +0200

    Debug XmlReader.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version='2.5.1',
     description='Python project who add automaticly all antenna from a subnet to a distant controller',
     py_modules=["unifiAdopter"],
-    package_dir={'': 'src'},
+    package_dir={'unifiAdopter': 'src'},
     packages=['controllers', 'workers', 'helpers', 'beans', 'files'],
     author="Simon Barras",
     author_email="simon.barras02@gmail.com",
